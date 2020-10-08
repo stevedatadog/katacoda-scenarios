908db59f84ed6c8f4045045a522320d161e194ca
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-08 12:58:10 -0700

    env vars between background/foreground

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -5,13 +5,13 @@ cd /ecommworkshop
 git pull master
 mv /root/docker-compose.yml /ecommworkshop/
 statusupdate setup
-
+statuscheck "environment variables"
 # Start storedog
-POSTGRES_USER=postgres POSTGRES_PASSWORD=postgres docker-compose up -d
+docker-compose --env-file ./docker.env up -d
 # Generate traffic
 ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000"
 # Break networking on discounts service
-docker run -it --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 loss ecommworkshop_discounts_1 --percent 85
+docker run -it --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 85
 statusupdate running
 
 statusupdate complete
--- dd101-npm/foreground.sh
+++ dd101-npm/foreground.sh
@@ -3,7 +3,12 @@ statuscheck tools
 statuscheck setup
 ln -s /ecommworkshop/ /root/lab
 cd /root/lab
-alias fixnetwork=docker stop pumba
+alias fixnetwork='docker stop pumba'
+printf "DD_API_KEY=$DD_API_KEY\n\
+DD_APP_KEY=$DD_APP_KEY\n\
+POSTGRES_USER=postgres\n\
+POSTGRES_PASSWORD=postgres" > docker.env 
+statusupdate "environment variables"
 statuscheck running
 clear
 prepenvironment
