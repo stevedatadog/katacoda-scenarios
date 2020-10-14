d534e5d4150517ddf440872f92aa1f2090456537
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-13 17:50:11 -0700

    more tweakery

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -16,8 +16,8 @@ do
   echo "waiting for discounts container..."
   sleep 5
 done
+statusupdate running
 # Break networking on discounts service.
 docker run -it --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 85
-statusupdate running
 
 statusupdate complete
