574ed792895c1118b1fb0c1c9285a860f97ad10b
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-13 18:16:57 -0700

    more

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -9,15 +9,14 @@ statuscheck "environment-variables"
 # Start storedog
 docker-compose --env-file ./docker.env up -d
 # Generate traffic
-./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000"
+./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" &
 # Wait for the discounts container to fire up
-#while [[ -z $(docker ps --filter "name=ecommworkshop_discounts_1" --format '{{.Names}}') ]]
-#do
-#  echo "waiting for discounts container..."
-#  sleep 5
-#done
-statusupdate running
+while [[ -z $(docker ps --filter "name=ecommworkshop_discounts_1" --format '{{.Names}}') ]]
+do
+  echo "waiting for discounts container..."
+  sleep 5
+done
 # Break networking on discounts service.
-#docker run -it --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 85
-
+docker run -it --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 85
+statusupdate running
 statusupdate complete
