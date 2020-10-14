ee8f6e48af5e740d73783a4ede59407f84ae5a0b
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-14 15:51:05 -0700

    moved GoR startup to end of background

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -8,8 +8,6 @@ statusupdate setup
 statuscheck "environment-variables"
 # Start storedog
 docker-compose --env-file ./docker.env up -d
-# Generate traffic
-./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" &
 # Wait for the discounts container to fire up
 while [[ -z $(docker ps --filter "name=ecommworkshop_discounts_1" --format '{{.Names}}') ]]
 do sleep 5
@@ -17,5 +15,7 @@ done
 sleep 5
 # Break networking on discounts service.
 docker run -it -d --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 75 
+# Generate traffic
+./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000"
 statusupdate running
 statusupdate complete
