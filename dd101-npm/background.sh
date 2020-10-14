33cb6aedd6f7847b0eac0b61981fc279326a3e9b
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-13 17:20:14 -0700

    further background script timing control

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -11,9 +11,10 @@ docker-compose --env-file ./docker.env up -d
 # Generate traffic
 ./gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000"
 # Wait for the discounts container to fire up
-while [[ $(docker ps --filter "name=ecommworkshop_discounts_1" --format '{{.Names}}') == "" ]]
-echo "waiting for discounts container..."
-do sleep 3
+while [[ -z $(docker ps --filter "name=ecommworkshop_discounts_1" --format '{{.Names}}') ]]
+do
+  echo "waiting for discounts container..."
+  sleep 5
 done
 # Break networking on discounts service.
 docker run -it --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 85
