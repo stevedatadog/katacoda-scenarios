165156c93a7284a15b161030a7cddbcbb746412b
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-19 23:56:05 -0700

    swap gor and pumba

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -15,9 +15,10 @@ do sleep 5
 done
 sleep 10
 
-# Generate traffic
-# /ecommworkshop/gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
-
 # Break networking on discounts service.
 docker run -it -d --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 75 
+
 statusupdate complete
+
+# Generate traffic
+/ecommworkshop/gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
