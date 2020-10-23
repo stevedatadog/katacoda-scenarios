0ba50eacaafc9793bc32710657e761c337a9d621
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-23 14:49:35 -0700

    more startup tweaks

--- dd101-npm/foreground.sh
+++ dd101-npm/foreground.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-statuscheck tools
 statuscheck setup
-cd /root/lab
 alias fixnetwork='docker stop pumba >> /dev/null 2>&1'
 printf "DD_API_KEY=$DD_API_KEY\n\
 DD_APP_KEY=$DD_APP_KEY\n\
 POSTGRES_USER=postgres\n\
-POSTGRES_PASSWORD=postgres" > docker.env 
+POSTGRES_PASSWORD=postgres" > /root/lab/docker.env 
 statusupdate "environment-variables"
 clear
