471a8446f5cd1846f73f004a1185d7a9c30ffbfa
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-26 17:49:35 -0700

    Another commit

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 git clone https://github.com/DataDog/ecommerce-workshop /root/lab && cd $_
-git checkout 9ce345 && git reset --hard
+# git checkout 9ce345 && git reset --hard
 mv /root/docker-compose.yml .
 docker-compose pull
 statusupdate setup
@@ -16,7 +16,7 @@ do sleep 5
 done
 
 # Break networking on discounts service.
-docker run -it -d --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info --skip-error netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 55 
+docker run -it -d --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info --skip-error netem --tc-image=gaiadocker/iproute2 --duration 90m loss lab_discounts_1 --percent 55 
 
 statusupdate complete
 
