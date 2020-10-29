84b2d58150e95a99704d38059963aa5ada47ea8b
Steve Calnan <steve.calnan@datadoghq.com>
2020-10-29 13:01:54 -0700

    Another commit

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
-git clone https://github.com/DataDog/ecommerce-workshop /root/lab && cd $_
-# git checkout 9ce345 && git reset --hard
-mv /root/docker-compose.yml .
-docker-compose pull
+statusupdate tools
+cd /ecommworkshop
+mv /root/docker-compose.yml /ecommworkshop/
+mv /root/ads-fixed.py /ecommworkshop/
+
+sed -i "/'ruby-shop'/ENV[\"DD_ENV\"]/" ./api/config/initializers/datadog.rb
+sed -i "/'ruby-shop'/ENV[\"DD_ENV\"]/" ./config/initializers/datadog.rb
+sed -i "/'ruby-shop'/ENV[\"DD_ENV\"]/" ./frontend/config/initializers/datadog.rb
+
 statusupdate setup
 
 # Start storedog
 statuscheck "environment-variables"
-docker-compose up -d
+docker-compose --env-file ./docker.env up -d
 
 # Wait for the discounts container to fire up
-while [[ -z $(docker ps --filter "name=lab_discounts_1" --format '{{.Names}}') ]]
+while [[ -z $(docker ps --filter "name=ecommworkshop_discounts_1" --format '{{.Names}}') ]]
 do sleep 5
 done
 
 # Break networking on discounts service.
-docker run -it -d --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info --skip-error netem --tc-image=gaiadocker/iproute2 --duration 90m loss lab_discounts_1 --percent 55 
+docker run -it -d --rm --name pumba -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba --log-level=info --skip-error netem --tc-image=gaiadocker/iproute2 --duration 90m loss ecommworkshop_discounts_1 --percent 55 
 
 statusupdate complete
 
 # Generate traffic
-/root/lab/gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
+/ecommworkshop/gor --input-file-loop --input-file requests_0.gor --output-http "http://localhost:3000" >> /dev/null 2>&1
--- dd101-npm/foreground.sh
+++ dd101-npm/foreground.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Wait for status tools 
-echo "waiting for tools..."
-while [[ -z $(which statuscheck) ]]; do sleep 5; done;
 statuscheck setup
+ln -s /ecommworkshop/ /root/lab
+cd /root/lab
 alias fixnetwork='docker stop pumba >> /dev/null 2>&1'
 printf "DD_API_KEY=$DD_API_KEY\n\
 DD_APP_KEY=$DD_APP_KEY\n\
 POSTGRES_USER=postgres\n\
-POSTGRES_PASSWORD=postgres" > /root/lab/.env 
-# while [[ -z $(which statusupdate) ]]; do sleep 5; done;
+POSTGRES_PASSWORD=postgres" > docker.env 
 statusupdate "environment-variables"
+clear
+prepenvironment
