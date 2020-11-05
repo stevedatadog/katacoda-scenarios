a96bd27e8535f4ee17ed8f8d0ca6baa7d0c05a0f
Steve Calnan <steve.calnan@datadoghq.com>
2020-11-05 10:49:23 -0800

    Another commit

--- dd101-npm/background.sh
+++ dd101-npm/background.sh
@@ -6,18 +6,18 @@ cd /ecommworkshop
 git fetch
 
 # fix 2.5 second delay in ads service
-# git checkout e400e3fc ./ads-service-fixed/ads.py
-# mv ./ads-service-fixed/ads.py ./ads-service/ads.py
+git checkout e400e3fc ./ads-service-fixed/ads.py
+mv ./ads-service-fixed/ads.py ./ads-service/ads.py
 
 # fix env tagging
-#sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/api/config/initializers/datadog.rb
-#sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/config/initializers/datadog.rb
-#sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/frontend/config/initializers/datadog.rb
+sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/api/config/initializers/datadog.rb
+sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/config/initializers/datadog.rb
+sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/frontend/config/initializers/datadog.rb
 
 # update ddtrace
-#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
-#sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
-#mv /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+mv /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
 
 statusupdate setup
 
