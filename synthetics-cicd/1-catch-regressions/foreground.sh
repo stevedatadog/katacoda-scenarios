c9043067382b534334ada47a08ebb55dc27bfd36
Steve Calnan <steve.calnan@datadoghq.com>
2021-01-21 14:39:04 -0800

    Removed strings

--- synthetics-cicd/1-catch-regressions/foreground.sh
+++ synthetics-cicd/1-catch-regressions/foreground.sh
@@ -3,10 +3,6 @@ statuscheck setup
 ln -s /ecommworkshop/ /root/lab
 cd /root/lab
 
-# For development only...
-export DD_API_KEY=***REMOVED***
-export DD_APP_KEY=***REMOVED***
-
 printf "DD_API_KEY=$DD_API_KEY\n\
 DD_APP_KEY=$DD_APP_KEY\n\
 POSTGRES_USER=postgres\n\
