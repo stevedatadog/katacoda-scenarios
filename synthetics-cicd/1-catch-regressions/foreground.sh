beeca2859d416e066d5ea540279684965430d022
Steve Calnan <steve.calnan@datadoghq.com>
2021-01-21 11:46:43 -0800

    Another commit

--- synthetics-cicd/1-catch-regressions/foreground.sh
+++ synthetics-cicd/1-catch-regressions/foreground.sh
@@ -1,1 +1,16 @@
-echo "This is the foreground."
+#!/bin/bash
+statuscheck setup
+ln -s /ecommworkshop/ /root/lab
+cd /root/lab
+
+# For development only...
+export DD_API_KEY=***REMOVED***
+export DD_APP_KEY=***REMOVED***
+
+printf "DD_API_KEY=$DD_API_KEY\n\
+DD_APP_KEY=$DD_APP_KEY\n\
+POSTGRES_USER=postgres\n\
+POSTGRES_PASSWORD=postgres" > docker.env 
+statusupdate "environment-variables"
+clear
+prepenvironment
