7793bbd483aeb3c050ab10ba5ae1f4511a505dba
Steve Calnan <steve.calnan@datadoghq.com>
2021-02-28 12:06:45 -0800

    more hacking

--- synthetics-cicd/2-cicd/background.sh
+++ synthetics-cicd/2-cicd/background.sh
@@ -3,6 +3,7 @@ SUBDOMAIN=$(cat /opt/.katacodasubdomain)
 KATACODAHOST=$(cat /opt/.katacodahost)
 
 ls -la /opt > /root/opt_list.txt
+env > /root/env.txt
 
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
@@ -33,8 +34,7 @@ drone-runner-exec service start
 apt-get install wait-for-it
 statusupdate "cicd-dependencies"
  
-# DRONE_GOGS_SERVER=https://$SUBDOMAIN-8300-$KATACODAHOST.environments.katacoda.com
-DRONE_GOGS_SERVER=drone-server:3000 # non-dd katacoda env
+DRONE_GOGS_SERVER=https://$SUBDOMAIN-8300-$KATACODAHOST.environments.katacoda.com
 echo "DRONE_GOGS_SERVER=$DRONE_GOGS_SERVER" > cicd-docker.env
 sed -i "s|REPLACE_WITH_GOGS_EXTERNAL_URL|$DRONE_GOGS_SERVER|g" gogs.app.ini
 statusupdate "cicd-environment"
--- synthetics-cicd/2-cicd/foreground.sh
+++ synthetics-cicd/2-cicd/foreground.sh
@@ -1,18 +1,18 @@
 # Doing a lot of extra work because DD images are broken
-git clone https://github.com/DataDog/ecommerce-workshop.git /ecommworkshop
-cd /ecommworkshop
-git checkout a670e6ab791d3ac4b9ef45dae1b68f64950414e4
-git reset --hard
+# git clone https://github.com/DataDog/ecommerce-workshop.git /ecommworkshop
+# cd /ecommworkshop
+# git checkout a670e6ab791d3ac4b9ef45dae1b68f64950414e4
+# git reset --hard
 ln -s /ecommworkshop /root/storedog
 cd /root/storedog
 
-curl http://illegalsystems.com/steve-envs.txt > storedog-docker.env
+# curl http://illegalsystems.com/steve-envs.txt > storedog-docker.env
 
-# printf "DD_API_KEY=$DD_API_KEY\n\
-# DD_APP_KEY=$DD_APP_KEY\n\
-# POSTGRES_USER=postgres\n\
-# POSTGRES_PASSWORD=postgres" > storedog-docker.env 
-statusupdate "environment-variables"
+printf "DD_API_KEY=$DD_API_KEY\n\
+DD_APP_KEY=$DD_APP_KEY\n\
+POSTGRES_USER=postgres\n\
+POSTGRES_PASSWORD=postgres" > storedog-docker.env 
+statusupdate "storedog-environment"
 
 echo "Setting up the lab..."
 statuscheck "cicd-dependencies"
