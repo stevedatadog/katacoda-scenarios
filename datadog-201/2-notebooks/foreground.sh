c06ea42ddabd2cdbe175ec1e717e02a957eb333a
Steve Calnan <steve.calnan@datadoghq.com>
2021-04-13 16:42:28 -0700

    Another commit

--- datadog-201/2-notebooks/foreground.sh
+++ datadog-201/2-notebooks/foreground.sh
@@ -11,3 +11,15 @@ cd ecommworkshop
 
 clear
 prepenvironment
+
+export DD_CLIENT_TOKEN=$(curl -s "https://api.datadoghq.com/api/v1/public_api_key" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+| jq  '.. | objects | select(.name == "[RUM] Storedog").hash')
+
+export DD_APPLICATION_ID=$(curl -s "https://api.datadoghq.com/api/v1/rum/projects" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
+| jq  '.. | objects | select(.name == "Storedog").application_id')
