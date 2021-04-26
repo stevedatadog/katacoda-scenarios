3294f79c96d36460f6ba2df1884224c90562a2f7
Steve Calnan <steve.calnan@datadoghq.com>
2021-04-25 19:29:48 -0700

    😽

--- datadog-201/2-notebooks/background.sh
+++ datadog-201/2-notebooks/background.sh
@@ -1,37 +1,32 @@
 # Idempotentiation
-if test -f "provisionedDatadog-201"; then
+if test -f "provisionedDD201"; then
   exit 0;
 fi
 
-# Get the latest lab tools
+#!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
-statusupdate tools
-
-# Return to simpler times
+mv /root/docker-compose.yml /ecommworkshop/
 cd /ecommworkshop
-git checkout 9ce34516d9a65d6f09a6fffd5c4911a409d31e3f 
+git fetch
 
-# Enable system probe for NPM
-sed -i "/- DD_TAGS='env:ruby-shop'/r"<(
-    echo "      - DD_SYSTEM_PROBE_ENABLED=true"
-    echo "    cap_add:"
-    echo "      - SYS_ADMIN"
-    echo "      - SYS_RESOURCE"
-    echo "      - SYS_PTRACE"
-    echo "      - NET_ADMIN"
-    echo "      - IPC_LOCK"
-    echo "    security_opt:"
-    echo "      - apparmor:unconfined"
-) /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml
-sed -i "/volumes:/a \ \ \ \ \ \ - /sys/kernel/debug:/sys/kernel/debug" /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml
+# fix 2.5 second delay in ads service
+git checkout e400e3fc ./ads-service-fixed/ads.py
+mv ./ads-service-fixed/ads.py ./ads-service/ads.py
 
-# workshop-specific environment
-sed -i "s/env:ruby-shop/env:dd201/" /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml
-cp /ecommworkshop/docker-compose-files/docker-compose-fixed-instrumented.yml /ecommworkshop/docker-compose.yml
-statusupdate configuration
+# fix env tagging
+sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/api/config/initializers/datadog.rb
+sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/config/initializers/datadog.rb
+sed -i 's/.ruby-shop./ENV["DD_ENV"]/' ./store-frontend-instrumented-fixed/frontend/config/initializers/datadog.rb
 
+# update ddtrace
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./ads-service/requirements.txt
+sed -i 's/ddtrace==0.28.0/ddtrace==0.41.0/g' ./discounts-service-fixed/requirements.txt
+mv /root/frontend-docker-entrypoint.sh ./store-frontend-instrumented-fixed/docker-entrypoint.sh
+
+statusupdate setup
+
+# Generate enriched logs
 statuscheck apikey
 DD_API_KEY=`cat /root/.dd_api_key` /root/postlogs.py 3600 &
 
-statusupdate logging
 statusupdate complete
--- datadog-201/2-notebooks/foreground.sh
+++ datadog-201/2-notebooks/foreground.sh
@@ -1,25 +1,16 @@
-# Make the app code available in the IDE
-statuscheck configuration
+statuscheck setup
 
-# Write API key for background.sh
+# Make the app code available in the IDE
+ln -s /ecommworkshop /root/lab
+cd lab
+
+# Pass user context to background
 echo $DD_API_KEY > /root/.dd_api_key && statusupdate apikey
 
-statuscheck logging
-
-ln -s /ecommworkshop /root/ecommworkshop
-cd ecommworkshop
+printf "DD_API_KEY=$DD_API_KEY\n\
+DD_APP_KEY=$DD_APP_KEY\n\
+POSTGRES_USER=postgres\n\
+POSTGRES_PASSWORD=postgres" > .env 
 
 clear
 prepenvironment
-
-export DD_CLIENT_TOKEN=$(curl -s "https://api.datadoghq.com/api/v1/public_api_key" \
--H "Content-Type: application/json" \
--H "DD-API-KEY: ${DD_API_KEY}" \
--H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
-| jq  '.. | objects | select(.name == "[RUM] Storedog").hash')
-
-export DD_APPLICATION_ID=$(curl -s "https://api.datadoghq.com/api/v1/rum/projects" \
--H "Content-Type: application/json" \
--H "DD-API-KEY: ${DD_API_KEY}" \
--H "DD-APPLICATION-KEY: ${DD_APP_KEY}" \
-| jq  '.. | objects | select(.name == "Storedog").application_id')
