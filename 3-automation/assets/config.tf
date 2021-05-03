747d289dd245ad3b58e71dd759ad03f10bb63d5a
Steve Calnan <steve.calnan@datadoghq.com>
2021-05-03 16:17:24 -0700

    😾

--- 3-automation/assets/config.tf
+++ 3-automation/assets/config.tf
@@ -9,27 +9,54 @@ terraform {
     }
 }
 
+variable "datadog_api_key" {
+    type = string
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "datadog_image" {
   name = "datadog/agent:7.21.1"
-  env = ["DD_API_KEY", "DD_LOGS_ENABLED=true", "DD_ENV=dd201"]
 }
 
 resource "docker_container" "datadog_container" {
   name = "datadog-agent"
   image = "${docker_image.datadog_image.name}"
+  env = [
+    "DD_API_KEY=${var.datadog_api_key}",
+    "DD_APM_NON_LOCAL_TRAFFIC=true",
+    "DD_LOGS_ENABLED=true",
+    "DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL=true",
+    "DD_ENV=dd201"
+  ]
 }
 
 resource "docker_container" "redis_container" {
   name = "redis-session-cache"
   image = "${docker_image.redis_image.name}"
+  env = [
+    "DD_SERVICE=redis-session-cache",
+    "DD_VERSION=1.0",
+    "DD_ENV=dd201"
+  ]
 }
 
 resource "docker_image" "redis_image" {
   name = "redis:6.2-alpine"
+  labels {
+      label = "com.datadoghq.ad.logs"
+      value = "[{\"source\": \"redis\", \"service\": \"redis-session-cache\"}]"
+  }
+  labels {
+      label = "com.datadoghq.tags.service"
+      value = "redis-session-cache"
+  }
+  labels {
+      label = "com.datadoghq.tags.env"
+      value = "dd201"
+  }
 }
 
 provider "datadog" {}
