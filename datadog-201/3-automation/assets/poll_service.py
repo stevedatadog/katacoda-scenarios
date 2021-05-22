0479cb4aaf9698d235b411524c0b23961d4e1c63
Steve Calnan <steve.calnan@datadoghq.com>
2021-05-21 17:19:08 -0700

    😾

--- datadog-201/3-automation/assets/poll_service.py
+++ datadog-201/3-automation/assets/poll_service.py
@@ -12,19 +12,18 @@ service=os.getenv('DD_SERVICE')
 
 tags = [ 'env:{tag}'.format(tag=environment),
          'service:{tag}'.format(tag=service),
-         'host:{tag}'.format(tag=host),
-         'source:python'
+         'host:{tag}'.format(tag=host)
 ]
 
 q_metric=os.getenv('DD_QUERY_METRIC')
-query='avg:{metric}{{tags}}'.format(metric=q_metric, tags=','.join(tags))
+query='avg:{metric}{{{tags}}}'.format(metric=q_metric, tags=','.join(tags))
 
 """ Poll the API for metrics
 """
 service_up=False
 while not service_up:
-    start_time=int(start=time.time())
-    end_time=end_time - 120
+    end_time=int(time.time())
+    start_time=end_time - 120
     response=api.Metric.query(start=start_time, end=end_time, query=query)
     print(response)
     time.sleep(2)
@@ -35,5 +34,4 @@ event_title='{service} is up'.format(service=service)
 event_text='The service polling script detected {metric} from {env} on {host}'.format(
     metric=q_metric, env=environment, service=service
 )
-api.Event.create(event_title, event_text, tags=tags)
-
+api.Event.create(event_title, event_text, tags=tags.append('source:python'))
