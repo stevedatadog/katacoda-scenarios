a9a5409773fcd6eb19002e5172b79f0894eb148d
Steve Calnan <steve.calnan@datadoghq.com>
2021-05-24 11:43:20 -0700

    😻

--- datadog-201/3-automation/4-terraform.md
+++ datadog-201/3-automation/4-terraform.md
@@ -1,4 +1,9 @@
-The Datadog provider for Terraform allows you to provision monitors and dashboards at the same time that you provision your system.
+If you have only a small cloud footprint, rarely provision new services, or you’re a one-person team, interacting with our API from ad hoc scripts and shell one-liners may be as much automation as you need. If you’re a growing team without a well-established workflow, or your scale has outgrown your existing tooling, an infrastructure-as-code (IaC) tool is worth looking into.
+
+Terraform is a popular IaC tool for managing infrastructure across many providers, on and off the cloud. The Datadog provider for Terraform allows you to provision monitors and dashboards at the same time that you provision your system.
+
+Load `lab/terraform/datadog.tf`{{open}} in the IDE to see how customer monitors and a dashboard can be defined in a Terraform configuration file.
+
 
 There is a terraform file that provisions a small web application called Stately. It consists of a falcon web server and a redis session cache. 
 
--- datadog-201/3-automation/assets/datadog.tf
+++ datadog-201/3-automation/assets/datadog.tf
@@ -1,7 +1,7 @@
 resource "datadog_monitor" "redis_cpu" {
   name = "Average Redis System CPU Usage"
   type = "metric alert"
-  message = "Uh oh. Redis is burning up the CPU! @steve.calnan@datadoghq.com"
+  message = "Uh oh. Redis is burning up the CPU! @sre@example.com"
   
   query = "avg(last_1h):avg:redis.cpu.sys{env:dd201,service:redis-session-cache} > 1"
 
@@ -13,4 +13,31 @@ resource "datadog_monitor" "redis_cpu" {
   }
 
   tags = ["env:dd201", "service:redis-session-cache"]
+}
+
+resource "datadog_dashboard" "redis_session_cache_dash" {
+  title         = "Redis Session Cache Dashboard"
+  description   = "Created using the Datadog provider in Terraform"
+  layout_type   = "ordered"
+  is_read_only  = true
+
+  widget {
+    alert_value_definition {
+      alert_id = "${datadaog_monitor.redis_cpu.id}"
+      text_align = "center"
+      title = "Redis System CPU Usage"
+    }
+  }
+
+  widget {
+    check_status_definition {
+      check = "datadog.agent.up"
+      grouping = "check"
+      group = "host:host01"
+      title = "Host Availability"
+      time = {
+        live_span = "1h"
+      }
+    }
+  }
 }
