16f700af3ee13e15502b5363686b2d0f49a3b95c
Steve Calnan <steve.calnan@datadoghq.com>
2021-06-27 22:44:24 -0700

    😹

--- api-automation-iac/1-meet-the-api/background.sh
+++ api-automation-iac/1-meet-the-api/background.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 curl -s https://datadoghq.dev/katacodalabtools/r?raw=true|bash
 
-mkdir -p /root/lab/stately
-mv /root/Dockerfile /root/lab/stately/
-mv /root/stately-server.py /root/lab/stately/
-mv /root/stately-index.html /root/lab/stately/index.html
-
-statusupdate setup
-
-docker build -t dd201/stately:1.0 /root/lab/stately/
-
-apt-get install wait-for-it
-
-statuscheck "write stately url"
+statuscheck "environment"
+source /root/env.sh
+curl -s -X POST https://api.datadoghq.com/api/v1/events \
+  -H "Content-Type: application/json" \
+  -H "DD-API-KEY: $DD_API_KEY" \
+  -H "DD-APPLICATION-KEY: $DD_APP_KEY" \
+  --data @/root/new_event.json
 
 statusupdate complete
-
-wait-for-it --timeout=3600 localhost:80
-
-STATELY_URL=$(cat /root/stately_url.txt)
-while true
-do
-  curl -s $STATELY_URL > /dev/null
-  sleep 1
-  curl -s -X POST $STATELY_URL/state \
-    -H "Content-Type: application/json" \
-    -H "X-Requested-With: cURL" > /dev/null
-  sleep 2
-done
--- api-automation-iac/1-meet-the-api/foreground.sh
+++ api-automation-iac/1-meet-the-api/foreground.sh
@@ -1,10 +1,7 @@
-clear
-mkdir /root/lab
-cd /root/lab
-
-STATELY_URL="https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
-echo $STATELY_URL > /root/stately_url.txt
-statusupdate "write stately url"
-
+#!/bin/bash
+# Pass environment vars to background.sh
+echo "export DD_API_KEY=$DD_API_KEY" > /root/env.sh
+echo "export DD_APP_KEY=$DD_APP_KEY" >> /root/env.sh
+statusupdate "environment"
 clear
 prepenvironment
